4e3b59813b78210a8b1e9f3620c41d5d73f5f25e
Albaitas <internet622@gmail.com>
2024-09-13 10:02:07 +0300

    change css to scss

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Lusitana } from 'next/font/google';
-import './globals.css';
+import './globals.scss';
 import Header from './components/Header';
 
 const inter = Lusitana({
